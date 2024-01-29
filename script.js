f8ab2821f426820113e10d87b9b6853488658187
Anurag Jaiswal <janurag582004@gmail.com>
2024-01-29 09:51:39 +0530

    .

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let lists = document.getElementsByClassName("list");
 let rightBox = document.getElementById("right");
 let leftBox = document.getElementById("left");
 
+
 for(list of lists){
     list.addEventListener("dragstart", function(e){
         let selected = e.target;
